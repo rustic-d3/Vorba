3c1b7012926e8b1ac6f6125f4636dfb59a5cef5a
Rustic Dumbravă <dumbravarustic454@gmail.com>
2026-09-10 21:01:54 +0300

    Move the session initialization in user_sesstion/local_storage.ts for a clean code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
 import "./App.css";
-import { useStorage, validSession } from "../user_session/local_storage";
-import { useEffect } from "react";
+import "../user_session/local_storage"
+import { initialiseSession } from "../user_session/local_storage";
+
 
 function App() {
-  const [sessionId, setSessionId] = useStorage("session_id");
-  const [sessionDate, setSessionDate] = useStorage("current_date");
-  const [gameStatus, setGameStatus] = useStorage("game_status");
-  const [currentRowIndex, setCurrentRowIndex] = useStorage("row_index");
-  const [guessList, setGuessList] = useStorage("guess_list");
-
-  useEffect(() => {
-    const myDate = new Date();
-    
-    if (!sessionId || !validSession(sessionDate) ) {
-      const randomStr = window.crypto.randomUUID();
-      setSessionId(randomStr);
-      setSessionDate(myDate.toISOString());
-      setGameStatus("in_progress");
-      setCurrentRowIndex("0");
-      setGuessList("[]");
-
-    }
-  }, [sessionId, setSessionId, setSessionDate, setGameStatus, setCurrentRowIndex, setGuessList]);
+  initialiseSession();
 
   return (
     <>
--- frontend/user_session/local_storage.ts
+++ frontend/user_session/local_storage.ts
@@ -13,20 +13,48 @@ export function useStorage(key: string) {
 
 export function validSession(inputDate: string | number | Date): boolean {
   const dateToCheck = new Date(inputDate);
-  
+
   if (isNaN(dateToCheck.getTime())) {
     return false;
   }
 
-  const options: Intl.DateTimeFormatOptions = { 
-    timeZone: 'Europe/Chisinau', 
-    year: 'numeric', 
-    month: 'numeric', 
-    day: 'numeric' 
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "Europe/Chisinau",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
   };
-  
-  const todayInChisinau = new Date().toLocaleDateString('en-US', options);
-  const inputInChisinau = dateToCheck.toLocaleDateString('en-US', options);
+
+  const todayInChisinau = new Date().toLocaleDateString("en-US", options);
+  const inputInChisinau = dateToCheck.toLocaleDateString("en-US", options);
 
   return todayInChisinau === inputInChisinau;
+}
+
+export function initialiseSession() {
+  const [sessionId, setSessionId] = useStorage("session_id");
+  const [sessionDate, setSessionDate] = useStorage("current_date");
+  const [gameStatus, setGameStatus] = useStorage("game_status");
+  const [currentRowIndex, setCurrentRowIndex] = useStorage("row_index");
+  const [guessList, setGuessList] = useStorage("guess_list");
+
+  useEffect(() => {
+    const myDate = new Date();
+
+    if (!sessionId || !validSession(sessionDate)) {
+      const randomStr = window.crypto.randomUUID();
+      setSessionId(randomStr);
+      setSessionDate(myDate.toISOString());
+      setGameStatus("in_progress");
+      setCurrentRowIndex("0");
+      setGuessList("[]");
+    }
+  }, [
+    sessionId,
+    setSessionId,
+    setSessionDate,
+    setGameStatus,
+    setCurrentRowIndex,
+    setGuessList,
+  ]);
 }
